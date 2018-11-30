b74003c396cbf203383bd6bdaaea6978f701a218
Camila QAT PC <camila.rodrigues@outlook.com.br>
2018-11-30 11:08:06 -0200

    Adicionando evento input ao text area e atualizando os contadores.

--- public/js/main.js
+++ public/js/main.js
@@ -3,3 +3,16 @@ var numPalavras = frase.split(" ").length;
 var tamanhoFrase = $("#tamanho-frase");
 
 tamanhoFrase.text(numPalavras);
+
+var campoDigitacao = $(".campo-digitacao");
+
+campoDigitacao.on("input", function() {
+    var conteudo = campoDigitacao.val();
+
+    var qtdPalavras = conteudo.split(/\s+/).length - 1;
+    $("#contador-palavras").text(qtdPalavras);
+
+    var conteudoSemEsapco = conteudo.replace(/\s+/g, "");
+    var qtdCaracteres = conteudoSemEsapco.length;
+    $("#contador-caracteres").text(qtdCaracteres);
+});
