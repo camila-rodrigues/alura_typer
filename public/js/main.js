673d0a628d61e4ad38d999f84982c92416e5cf39
Camila QAT PC <camila.rodrigues@outlook.com.br>
2018-12-04 18:03:05 -0200

    Adicionando tabela com o placar do jogo, com a capacidade de deletar o registro da tabela.

--- public/js/main.js
+++ public/js/main.js
@@ -43,13 +43,18 @@ function inicializaCronometro() {
 
             if (tempoRestante < 1)
             {
-                campoDigitacao.attr("disabled", true);
                 clearInterval(cronometroID);
-                $("#botao-reiniciar").attr("disabled", false);
-                campoDigitacao.toggleClass("campo-desativado");
+                finalizaJogo();
             }
         }, 1000);
     });
+}
+
+function finalizaJogo() {
+    campoDigitacao.attr("disabled", true);
+    $("#botao-reiniciar").attr("disabled", false);
+    campoDigitacao.toggleClass("campo-desativado");
+    inserePlacar();
 }
 
 function inicializaMarcadores() {
@@ -69,6 +74,43 @@ function inicializaMarcadores() {
             campoDigitacao.removeClass("campo-certo");
         }
     });
+}
+
+function inserePlacar() {
+    var corpoTabela = $(".placar").find("tbody");
+    var usuario = "Camila";
+    var numPalavras = $("#contador-palavras").text();
+
+    var linha = novaLinha(usuario, numPalavras);
+
+    linha.find(".botao-remover").click(removeLinha);
+    
+    corpoTabela.prepend(linha);
+}
+
+function novaLinha(usuario, numPalavras) {
+    var linha = $("<tr>");
+    var colunaUsuario = $("<td>").text(usuario);
+    var colunaPalavras = $("<td>").text(numPalavras);
+    var colunaRemover = $("<td>");
+
+    var link = $("<a>").addClass("botao-remover").attr("href", "#");
+    var icone = $("<i>").addClass("small").addClass("material-icons").text("delete");
+
+    link.append(icone);
+
+    colunaRemover.append(link);
+
+    linha.append(colunaUsuario);
+    linha.append(colunaPalavras);
+    linha.append(colunaRemover);
+
+    return linha;
+}
+
+function removeLinha(event) {
+    event.preventDefault();
+    $(this).parent().parent().remove();
 }
 
 function reiniciaJogo() {
