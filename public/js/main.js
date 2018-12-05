d528a5d6975b5266564e0937889e226d85a2ae46
Camila QAT PC <camila.rodrigues@outlook.com.br>
2018-12-05 11:42:35 -0200

    Separando responsabilidades em arquivos js especificos. Adicionado um arquivo de JavaScript chamado placar para separar todas as responsabilidades do mesmo.

--- public/js/main.js
+++ public/js/main.js
@@ -76,43 +76,6 @@ function inicializaMarcadores() {
     });
 }
 
-function inserePlacar() {
-    var corpoTabela = $(".placar").find("tbody");
-    var usuario = "Camila";
-    var numPalavras = $("#contador-palavras").text();
-
-    var linha = novaLinha(usuario, numPalavras);
-
-    linha.find(".botao-remover").click(removeLinha);
-    
-    corpoTabela.prepend(linha);
-}
-
-function novaLinha(usuario, numPalavras) {
-    var linha = $("<tr>");
-    var colunaUsuario = $("<td>").text(usuario);
-    var colunaPalavras = $("<td>").text(numPalavras);
-    var colunaRemover = $("<td>");
-
-    var link = $("<a>").addClass("botao-remover").attr("href", "#");
-    var icone = $("<i>").addClass("small").addClass("material-icons").text("delete");
-
-    link.append(icone);
-
-    colunaRemover.append(link);
-
-    linha.append(colunaUsuario);
-    linha.append(colunaPalavras);
-    linha.append(colunaRemover);
-
-    return linha;
-}
-
-function removeLinha(event) {
-    event.preventDefault();
-    $(this).parent().parent().remove();
-}
-
 function reiniciaJogo() {
     campoDigitacao.attr("disabled", false);
     campoDigitacao.val("");
