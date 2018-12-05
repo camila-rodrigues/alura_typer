9e148af2f45af653084759aa54d1b37856af2245
Camila Rorigues <camila.rodrigues@outlook.com.br>
2018-12-05 19:19:12 -0200

    Adicionando botao para exibir ou ocultar o placar. Utilizando a função slideToggle, para esmaecer e não simplemente esconde bruscamente como se ulizasse hide, show ou toggle.

--- public/js/placar.js
+++ public/js/placar.js
@@ -34,3 +34,9 @@ function removeLinha(event) {
     event.preventDefault();
     $(this).parent().parent().remove();
 }
+
+$("#botao-placar").click(mostraPlacar);
+
+function mostraPlacar() {
+    $(".placar").stop().slideToggle(600);
+}
